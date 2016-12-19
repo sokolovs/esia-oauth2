8639d60c40882dca3447eb9069d4316719c00753
Сергей Соколов <sokol@zavolga.net>
2016-12-20 01:12:17 +0300

    Setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='esia-oauth2',
+    version='1.0',
+    description='ESIA OAuth2 Connector. Based on esia-connector [https://github.com/eigenmethod/esia-connector/]',
+    author='Sergey V. Sokolov',
+    author_email='sokolov@aksys.pro',
+    url='https://github.com/sokolovs/esia-oauth2',
+    packages=['esia',],
+)
